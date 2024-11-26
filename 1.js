2c881563702fc72a0e160a806f2637be867260d5
xy <2155586608@qq.com>
2024-11-26 14:36:01 +0800

    tow

---
+++ 1.js
@@ -0,0 +1,10 @@
+var one = prompt("请输入一个整数","")
+    if(Number(one) % 2 == 0){
+        if(Number(one) <= 1){
+            alert("奇数")
+        } else {
+            alert("偶数")
+        }
+    } else {
+        alert("奇数")
+    }
